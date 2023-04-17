bd357863ddbe9b59e6a8570e1a003fd0ca441abc
SanzidaTasnim <87225133+SanzidaTasnim@users.noreply.github.com>
2023-04-17 21:24:10 +0600

    Update script.js

--- Module11/script.js
+++ Module11/script.js
@@ -1,7 +1,7 @@
 function multiplicationTable(num){
    for(let i = 1; i <= 10; i++){
       let product = num * i;
-      document.write(num + " x " + i + " = " + product + "<br/>");
+      console.log(num + " x " + i + " = " + product + "<br/>");
    }
    
 }
